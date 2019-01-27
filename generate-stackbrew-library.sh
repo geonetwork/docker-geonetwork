40ed4dce1cba141b69baffd2eac833e7f59d12aa
doublebyte1 <doublebyte@gmail.com>
2019-01-27 14:50:47 +0100

    - Set version 3.6.0 as the official version of GN.

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -4,11 +4,12 @@ set -eu
 declare -A aliases=(
         [3.0.5]='3.0'
         [3.2.2]='3.2'
-        [3.4.4]='3.4 latest'
+        [3.4.4]='3.4'
+        [3.6.0]='3.6 latest'
 )
 
 # builds to exclude from tagging
-dirExclude=([3.0.5],[3.2.0],[3.2.1],[3.2.2],[3.4.0],[3.4.1],[3.4.2],[3.4.3])
+dirExclude=([3.0.5],[3.2.0],[3.2.1],[3.2.2],[3.4.0],[3.4.1],[3.4.2],[3.4.3],[3.4.4])
 
 self="$(basename "$BASH_SOURCE")"
 cd "$(dirname "$(readlink "$BASH_SOURCE")")"
