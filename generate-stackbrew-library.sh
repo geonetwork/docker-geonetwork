7fa2e3de056490ef677313333d2320629309e360
Juan Luis Rodríguez Ponce <juanluisrp@geocat.net>
2019-11-04 11:08:31 +0100

    Follow parent architectures (#36)
    Use same architectures set than parent image.
    
    Automatically retrieve and use the architectures set defined by the parent image (currently tomcat:8.5-jdk8).
    Inspired by official tomcat image https://github.com/docker-library/tomcat/blob/8348a86101660e76224afcc57c4c9fae4dc76de8/generate-stackbrew-library.sh#L47-L61

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -41,15 +41,6 @@ dirCommit() {
 	)
 }
 
-cat <<-EOH
-# this file is generated via https://github.com/geonetwork/docker-geonetwork/blob/$(fileCommit "$self")/$self
-
-Maintainers: Joana Simoes <jo@doublebyte.net> (@doublebyte1),
-	     Juan Luis Rodriguez <juanluisrp@geocat.net> (@juanluisrp)
-GitRepo: https://github.com/geonetwork/docker-geonetwork
-Architectures: amd64
-EOH
-
 # prints "$2$1$3$1...$N"
 join() {
 	local sep="$1"; shift
@@ -57,10 +48,36 @@ join() {
 	echo "${out#$sep}"
 }
 
+getArches() {
+	local repo="$1"; shift
+	local officialImagesUrl='https://github.com/docker-library/official-images/raw/master/library/'
+
+	eval "declare -g -A parentRepoToArches=( $(
+		find -name 'Dockerfile' -exec awk '
+				toupper($1) == "FROM" && $2 !~ /^('"$repo"'|scratch|.*\/.*)(:|$)/ {
+					print "'"$officialImagesUrl"'" $2
+				}
+			' '{}' + \
+			| sort -u \
+			| xargs bashbrew cat --format '[{{ .RepoName }}:{{ .TagName }}]="{{ join " " .TagEntry.Architectures }}"'
+	) )"
+}
+getArches 'geonetwork'
+
+cat <<-EOH
+# this file is generated via https://github.com/geonetwork/docker-geonetwork/blob/$(fileCommit "$self")/$self
+
+Maintainers: Joana Simoes <jo@doublebyte.net> (@doublebyte1),
+	     Juan Luis Rodriguez <juanluisrp@geocat.net> (@juanluisrp)
+GitRepo: https://github.com/geonetwork/docker-geonetwork
+EOH
+
+
 for version in "${versions[@]}"; do
   if ! (echo ${dirExclude[@]} | grep -w $version > /dev/null) ; then
 
 	commit="$(dirCommit "$version")"
+	dir="$version"
 
 	fullVersion="$(git show "$commit":"$version/Dockerfile" | awk '$1 == "ENV" && $2 == "GN_VERSION" { print $3; exit }')"
 
@@ -69,12 +86,19 @@ for version in "${versions[@]}"; do
 		${aliases[$version]:-}
 	)
 
+	variantParent="$(awk 'toupper($1) == "FROM" { print $2 }' "$dir/Dockerfile")"
+	[ -n "$variantParent" ]
+	variantArches="${parentRepoToArches[$variantParent]}"
+	
 	echo
 	cat <<-EOE
 		Tags: $(join ', ' "${versionAliases[@]}")
+		Architectures: $(join ', ' $variantArches)
 		GitCommit: $commit
 		Directory: $version
 	EOE
+	constraints="$(bashbrew cat --format '{{ .TagEntry.Constraints | join ", " }}' "https://github.com/docker-library/official-images/raw/master/library/$variantParent")"
+			[ -z "$constraints" ] || echo "Constraints: $constraints"
 
 	for variant in postgres; do
 		[ -f "$version/$variant/Dockerfile" ] || continue
@@ -83,13 +107,19 @@ for version in "${versions[@]}"; do
 
 		variantAliases=( "${versionAliases[@]/%/-$variant}" )
 		variantAliases=( "${variantAliases[@]//latest-/}" )
+		variantParent="$(awk 'toupper($1) == "FROM" { print $2 }' "$dir/Dockerfile")"
+		[ -n "$variantParent" ]
 
 		echo
 		cat <<-EOE
 			Tags: $(join ', ' "${variantAliases[@]}")
+			Architectures: $(join ', ' $variantArches)
 			GitCommit: $commit
 			Directory: $version/$variant
 		EOE
+
+		constraints="$(bashbrew cat --format '{{ .TagEntry.Constraints | join ", " }}' "https://github.com/docker-library/official-images/raw/master/library/$variantParent")"
+			[ -z "$constraints" ] || echo "Constraints: $constraints"
 	done
   fi
 done
