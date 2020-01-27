9fcf08514e1cbcd6b4f48a1d6acfac9fd23afa2d
Juan Luis Rodríguez <juanluisrp@gmail.com>
2020-01-27 21:11:05 +0100

    Remove 3.6.0 tag

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -12,7 +12,7 @@ declare -A aliases=(
 )
 
 # builds to exclude from tagging
-dirExclude=([3.0.5],[3.2.0],[3.2.1],[3.2.2],[3.4.0],[3.4.1],[3.4.2],[3.4.3],[3.4.4],[3.8.0],[3.8.1],[3.8.2])
+dirExclude=([3.0.5],[3.2.0],[3.2.1],[3.2.2],[3.4.0],[3.4.1],[3.4.2],[3.4.3],[3.4.4],[3.6.0],[3.8.0],[3.8.1],[3.8.2])
 
 self="$(basename "$BASH_SOURCE")"
 cd "$(dirname "$(readlink "$BASH_SOURCE")")"
