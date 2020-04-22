1d05db74078077c57d17a4754d833dba107c923f
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2020-04-22 11:11:42 +0200

    customizing the entrypoint to be db-agnostic
    * if POSTGRES_DB_HOST is enabled, then customize the webapp at runtime
      to configure an external postgresql
    * if not, stick with embedded h2

--- 3.99.0/README.md
+++ 3.99.0/README.md
@@ -23,3 +23,16 @@ docker-compose build && docker-compose up
 
 5. Open http://localhost:8080/geonetwork/ in a browser
 
+# Dabase configuration
+
+The provided docker-compose shows an example of using GeoNetwork with an
+external database (based on the `mdillon/postgis` docker image). If the
+environment variables related to the database from the `geonetwork` service are
+not defined, then the geonetwork will launch with an integrated h2 database.
+
+As soon as the `POSTGRES_DB_HOST` variable is defined on the `geonetwork`
+service, the docker entrypoint will reconfigure at runtime to target this type
+of database, and it will require the other related env variables to be defined
+as well (username, password, db name and so on).
+
+
--- 3.99.0/docker-entrypoint.sh
+++ 3.99.0/docker-entrypoint.sh
@@ -3,35 +3,70 @@ set -e
 
 if [ "$1" = 'catalina.sh' ]; then
 
-	mkdir -p "$DATA_DIR"
+    mkdir -p "$DATA_DIR"
 
-	#Set geonetwork data dir
-	export CATALINA_OPTS="$CATALINA_OPTS -Dgeonetwork.dir=$DATA_DIR"
-fi
+    #Set geonetwork data dir
+    export CATALINA_OPTS="$CATALINA_OPTS -Dgeonetwork.dir=$DATA_DIR"
 
-# Reconfigure Elasticsearch & Kibana if necessary
-if [ "$ES_HOST" != "localhost" ]; then
-  sed -i "s#http://localhost:9200#${ES_PROTOCOL}://${ES_HOST}:${ES_PORT}#g" $CATALINA_HOME/webapps/geonetwork/WEB-INF/web.xml ;
-  sed -i "s#es.host=localhost#es.host=${ES_HOST}#" $CATALINA_HOME/webapps/geonetwork/WEB-INF/config.properties ;
-fi;
+    # postgresql mode enabled: reconfigure the webapp to enable postgres configuration
 
-if [ "$ES_PROTOCOL" != "http" ] ; then
-  sed -i "s#es.protocol=http#es.protocol=${ES_PROTOCOL}#" $CATALINA_HOME/webapps/geonetwork/WEB-INF/config.properties ;
-fi
+    if [ $POSTGRES_DB_HOST != "" ]; then
+        #Setting host (use $POSTGRES_DB_HOST if it's set, otherwise use "postgres")
+        db_host="${POSTGRES_DB_HOST:-postgres}"
+        echo "db host: $db_host"
 
-if [ "$ES_PORT" != "9200" ] ; then
-  sed -i "s#es.port=9200#es.port=${ES_PORT}#" $CATALINA_HOME/webapps/geonetwork/WEB-INF/config.properties ;
-fi
-if [ "$ES_USERNAME" != "" ] ; then
-  sed -i "s#es.username=#es.username=${ES_USERNAME}#" $CATALINA_HOME/webapps/geonetwork/WEB-INF/config.properties ;
-fi
-if [ "$ES_PASSWORD" != "" ] ; then
-  sed -i "s#es.password=#es.password=${ES_PASSWORD}#" $CATALINA_HOME/webapps/geonetwork/WEB-INF/config.properties ;
-fi
+        #Setting port
+        db_port="${POSTGRES_DB_PORT:-5432}"
+        echo "db port: $db_port"
 
-if [ "$KB_URL" != "http://localhost:5601" ]; then
-  sed -i "s#kb.url=http://localhost:5601#kb.url=${KB_URL}#" $CATALINA_HOME/webapps/geonetwork/WEB-INF/config.properties ;
-  sed -i "s#http://localhost:5601#${KB_URL}#g" $CATALINA_HOME/webapps/geonetwork/WEB-INF/web.xml ;
+        if [ -z "$POSTGRES_DB_USERNAME" ] || [ -z "$POSTGRES_DB_PASSWORD" ]; then
+            echo >&2 "you must set POSTGRES_DB_USERNAME and POSTGRES_DB_PASSWORD"
+            exit 1
+        fi
+
+        db_gn="${POSTGRES_DB_NAME:-geonetwork}"
+
+        #Write connection string for GN
+        sed -ri '/^jdbc[.](username|password|database|host|port)=/d' "$CATALINA_HOME"/webapps/geonetwork/WEB-INF/config-db/jdbc.properties
+        echo "jdbc.username=$POSTGRES_DB_USERNAME" >> "$CATALINA_HOME"/webapps/geonetwork/WEB-INF/config-db/jdbc.properties
+        echo "jdbc.password=$POSTGRES_DB_PASSWORD" >> "$CATALINA_HOME"/webapps/geonetwork/WEB-INF/config-db/jdbc.properties
+        echo "jdbc.database=$db_gn" >> "$CATALINA_HOME"/webapps/geonetwork/WEB-INF/config-db/jdbc.properties
+        echo "jdbc.host=$db_host" >> "$CATALINA_HOME"/webapps/geonetwork/WEB-INF/config-db/jdbc.properties
+        echo "jdbc.port=$db_port" >> "$CATALINA_HOME"/webapps/geonetwork/WEB-INF/config-db/jdbc.properties
+
+        #Fixing an hardcoded port on the connection string (bug fixed on development branch)
+        sed -i -e 's#5432#${jdbc.port}#g' $CATALINA_HOME/webapps/geonetwork/WEB-INF/config-db/postgres.xml
+
+        # reconfigure h2 -> postgres
+        sed -i -e 's#<import resource="../config-db/h2.xml"/>#<!--<import resource="../config-db/h2.xml"/> -->#g' $CATALINA_HOME/webapps/geonetwork/WEB-INF/config-node/srv.xml
+        sed -i -e 's#<!--<import resource="../config-db/postgres.xml"/>-->#<import resource="../config-db/postgres.xml"/>#g' $CATALINA_HOME/webapps/geonetwork/WEB-INF/config-node/srv.xml
+
+    fi
+
+    # Reconfigure Elasticsearch & Kibana if necessary
+    if [ "$ES_HOST" != "localhost" ]; then
+        sed -i "s#http://localhost:9200#${ES_PROTOCOL}://${ES_HOST}:${ES_PORT}#g" $CATALINA_HOME/webapps/geonetwork/WEB-INF/web.xml ;
+        sed -i "s#es.host=localhost#es.host=${ES_HOST}#" $CATALINA_HOME/webapps/geonetwork/WEB-INF/config.properties ;
+    fi;
+
+    if [ "$ES_PROTOCOL" != "http" ] ; then
+        sed -i "s#es.protocol=http#es.protocol=${ES_PROTOCOL}#" $CATALINA_HOME/webapps/geonetwork/WEB-INF/config.properties ;
+    fi
+
+    if [ "$ES_PORT" != "9200" ] ; then
+        sed -i "s#es.port=9200#es.port=${ES_PORT}#" $CATALINA_HOME/webapps/geonetwork/WEB-INF/config.properties ;
+    fi
+    if [ "$ES_USERNAME" != "" ] ; then
+        sed -i "s#es.username=#es.username=${ES_USERNAME}#" $CATALINA_HOME/webapps/geonetwork/WEB-INF/config.properties ;
+    fi
+    if [ "$ES_PASSWORD" != "" ] ; then
+        sed -i "s#es.password=#es.password=${ES_PASSWORD}#" $CATALINA_HOME/webapps/geonetwork/WEB-INF/config.properties ;
+    fi
+
+    if [ "$KB_URL" != "http://localhost:5601" ]; then
+        sed -i "s#kb.url=http://localhost:5601#kb.url=${KB_URL}#" $CATALINA_HOME/webapps/geonetwork/WEB-INF/config.properties ;
+        sed -i "s#http://localhost:5601#${KB_URL}#g" $CATALINA_HOME/webapps/geonetwork/WEB-INF/web.xml ;
+    fi
 fi
 
 exec "$@"
