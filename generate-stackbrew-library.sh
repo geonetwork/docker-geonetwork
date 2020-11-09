4475831219cebead1c57d1b72feda488c26788f5
Juan Luis Rodríguez <juanluisrp@gmail.com>
2020-11-09 12:25:39 +0100

    Release of GeoNetwork 3.10.5

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -7,13 +7,13 @@ declare -A aliases=(
         [3.4.4]='3.4'
         [3.6.0]='3.6'
         [3.8.3]='3.8'
-		[3.10.4]='3.10 3 latest'
+		[3.10.5]='3.10 3 latest'
 		[4.0.0]='4.0 4'
 
 )
 
 # builds to exclude from tagging
-dirExclude=([3.0.5],[3.2.0],[3.2.1],[3.2.2],[3.4.0],[3.4.1],[3.4.2],[3.4.3],[3.4.4],[3.6.0],[3.8.0],[3.8.1],[3.8.2],[3.10.0],[3.10.1],[3.10.2],[3.10.3],[4.0.0-alpha.1],[4.0.0-alpha.2])
+dirExclude=([3.0.5],[3.2.0],[3.2.1],[3.2.2],[3.4.0],[3.4.1],[3.4.2],[3.4.3],[3.4.4],[3.6.0],[3.8.0],[3.8.1],[3.8.2],[3.10.0],[3.10.1],[3.10.2],[3.10.3],[3.10.4],[4.0.0-alpha.1],[4.0.0-alpha.2])
 
 self="$(basename "$BASH_SOURCE")"
 cd "$(dirname "$(readlink "$BASH_SOURCE")")"
@@ -76,7 +76,7 @@ EOH
 
 
 for version in "${versions[@]}"; do
-  if ! (echo ${dirExclude[@]} | grep -w $version > /dev/null) ; then
+  if ! (echo ${dirExclude[@]} | grep -w "\[$version\]" > /dev/null) ; then
 
 	commit="$(dirCommit "$version")"
 	dir="$version"
