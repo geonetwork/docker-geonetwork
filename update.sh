d374d52387343a3a99700f57dc3f015f5c97714f
Juan Luis Rodríguez <juanluisrp@gmail.com>
2023-03-07 18:03:49 +0100

    Update next version script

--- update.sh
+++ update.sh
@@ -9,14 +9,29 @@ if [ ${#versions[@]} -eq 0 ]; then
 fi
 versions=( "${versions[@]%/}" )
 
+
 for version in "${versions[@]}"; do
 	echo "Updating Dockerfile for ${version} version"
 	md5="$(curl -fsSL --retry 5 "https://sourceforge.net/projects/geonetwork/files/GeoNetwork_opensource/v${version}/geonetwork.war.md5/download" | awk '{print $1;}')"
 	echo "MD5 for GeoNetwork ${version} is ${md5}"
 
+	echo "Updating ${version}/Dockerfile"
 	sed -ri \
 		-e 's/^(ENV GN_VERSION) .*/\1 '"$version"'/' \
 		-e 's/^(ENV GN_DOWNLOAD_MD5) .*/\1 '"$md5"'/' \
-		-e 's/^(FROM geonetwork):.*/\1:'"$version"'/' \
-		"$version/Dockerfile" "$version/postgres/Dockerfile"
+		"$version/Dockerfile"
+
+	if [[ -f "$version/postgres/Dockerfile" ]]; then 
+		echo "Updating ${version}/postgres/Dockerfile"
+		sed -ri \
+			-e 's/^(FROM geonetwork):.*/\1:'"$version"'/' \
+			"$version/postgres/Dockerfile"
+	fi
+
+	if [[  -f "$version/docker-compose.yml" ]]; then
+		echo "Updating microservices reference"
+		sed -ri \
+			-e "s#geonetwork/gn-cloud-ogc-api-records-service:.*#geonetwork/gn-cloud-ogc-api-records-service:${version}-0#" \
+			"$version/docker-compose.yml"
+	fi
 done
