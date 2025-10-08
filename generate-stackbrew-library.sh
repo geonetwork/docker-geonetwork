69cb85ef18f6fe11ad4c9f848e47e307757199a8
Juan Luis Rodríguez <juanluisrp@gmail.com>
2025-10-08 09:47:46 +0200

    Refactor generate-stackbrew-library.sh to use associative arrays for dirExclude and check Bash version
    - Replaced the dirExclude bash array with an associative array for more efficient lookups and easier exclusion checks.
    - Added a Bash version check at the start of the script to ensure Bash 4+ is used (required for associative arrays).
    - Updated all exclusion logic to use the associative array style: `if [[ -z ${dirExclude[$version]+x} ]]; then ...`.
    - Improved getArches function to build the eval string properly while checking exclusions using the associative array.
    - Fixed variantParent assignment for variant builds to use the correct Dockerfile.
    - Made minor formatting and whitespace adjustments for readability and consistency.
    - Constraints check is now error-tolerant (`|| true`) to avoid script failures if bashbrew returns an error.
    - Script logic and output remain unchanged; this refactor improves performance and code maintainability.

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 set -eu
 
+# Bash 4+ required for associative arrays
+if ((BASH_VERSINFO[0] < 4)); then
+    echo "Bash 4 or above is required."
+    exit 1
+fi
+
 declare -A aliases=(
-        [3.0.5]='3.0'
-        [3.2.2]='3.2'
-        [3.4.4]='3.4'
-        [3.6.0]='3.6'
-        [3.8.3]='3.8'
-        [3.10.10]='3.10'
-        [3.12.12]='3.12 3'
-        [4.2.14]='4.2'
-        [4.4.9]='4.4 4 latest'
+    [3.0.5]='3.0'
+    [3.2.2]='3.2'
+    [3.4.4]='3.4'
+    [3.6.0]='3.6'
+    [3.8.3]='3.8'
+    [3.10.10]='3.10'
+    [3.12.12]='3.12 3'
+    [4.2.14]='4.2'
+    [4.4.9]='4.4 4 latest'
 )
 
-# builds to exclude from tagging
-dirExclude=([3.0.5],[3.2.0],[3.2.1],[3.2.2],[3.4.0],[3.4.1],[3.4.2],[3.4.3],[3.4.4],[3.6.0],[3.8.0],[3.8.1],[3.8.2],[3.8.3],[3.10.0],[3.10.1],[3.10.2],[3.10.3],[3.10.4],[3.10.5],[3.10.6],[3.10.7],[3.10.8],[3.10.9],[3.10.10],[3.12.0],[3.12.1],[3.12.2],[3.12.3],[3.12.4],[3.12.5],[3.12.6],[3.12.7],[3.12.8],[3.12.9],[3.12.10],[3.12.11],[4.0.0-alpha.1],[4.0.0-alpha.2],[4.0.0],[4.0.1],[4.0.2],[4.0.3],[4.0.4],[4.0.5],[4.0.6],[4.2.0],[4.2.1],[4.2.2],[4.2.3],[4.2.4],[4.2.5],[4.2.6],[4.2.7],[4.2.8],[4.2.9],[4.2.10],[4.2.11],[4.2.12],[4.2.13],[4.4.0],[4.4.1],[4.4.2],[4.4.3],[4.4.4],[4.4.5],[4.4.6],[4.4.7],[4.4.8])
-
+declare -A dirExclude=(
+    [3.0.5]=1 
+	[3.2.0]=1 [3.2.1]=1 [3.2.2]=1 [3.4.0]=1 [3.4.1]=1 [3.4.2]=1 [3.4.3]=1 [3.4.4]=1
+    [3.6.0]=1 [3.8.0]=1 [3.8.1]=1 [3.8.2]=1 [3.8.3]=1 
+	[3.10.0]=1 [3.10.1]=1 [3.10.2]=1 [3.10.3]=1 [3.10.4]=1 [3.10.5]=1 [3.10.6]=1 [3.10.7]=1 [3.10.8]=1 [3.10.9]=1 [3.10.10]=1 [3.12.0]=1 [3.12.1]=1
+    [3.12.2]=1 [3.12.3]=1 [3.12.4]=1 [3.12.5]=1 [3.12.6]=1 [3.12.7]=1 [3.12.8]=1 [3.12.9]=1 [3.12.10]=1 [3.12.11]=1 
+	[4.0.0-alpha.1]=1 [4.0.0-alpha.2]=1 [4.0.0]=1 [4.0.1]=1 [4.0.2]=1 [4.0.3]=1 [4.0.4]=1 [4.0.5]=1 [4.0.6]=1 
+	[4.2.0]=1 [4.2.1]=1 [4.2.2]=1 [4.2.3]=1 [4.2.4]=1 [4.2.5]=1 [4.2.6]=1 [4.2.7]=1 [4.2.8]=1 [4.2.9]=1 [4.2.10]=1 [4.2.11]=1 [4.2.12]=1 [4.2.13]=1 
+	[4.4.0]=1 [4.4.1]=1 [4.4.2]=1 [4.4.3]=1 [4.4.4]=1 [4.4.5]=1 [4.4.6]=1 [4.4.7]=1 [4.4.8]=1
+)
 
 self="$(basename "$BASH_SOURCE")"
 cd "$(dirname "$(readlink "$BASH_SOURCE")")"
@@ -23,52 +36,51 @@ cd "$(dirname "$(readlink "$BASH_SOURCE")")"
 versions=( */ )
 versions=( "${versions[@]%/}" )
 
-# get the most recent commit which modified any of "$@"
 fileCommit() {
-	git log -1 --format='format:%H' HEAD -- "$@"
+    git log -1 --format='format:%H' HEAD -- "$@"
 }
 
-# get the most recent commit which modified "$1/Dockerfile" or any file COPY'd from "$1/Dockerfile"
 dirCommit() {
-	local dir="$1"; shift
-	(
-		cd "$dir"
-		fileCommit \
-			Dockerfile \
-			$(git show HEAD:./Dockerfile | awk '
-				toupper($1) == "COPY" {
-					for (i = 2; i < NF; i++) {
-						print $i
-					}
-				}
-			')
-	)
+    local dir="$1"; shift
+    (
+        cd "$dir"
+        fileCommit \
+            Dockerfile \
+            $(git show HEAD:./Dockerfile | awk '
+                toupper($1) == "COPY" {
+                    for (i = 2; i < NF; i++) { print $i }
+                }
+            ')
+    )
 }
 
-# prints "$2$1$3$1...$N"
 join() {
-	local sep="$1"; shift
-	local out; printf -v out "${sep//%/%%}%s" "$@"
-	echo "${out#$sep}"
+    local sep="$1"; shift
+    local out; printf -v out "${sep//%/%%}%s" "$@"
+    echo "${out#$sep}"
 }
 
 getArches() {
-	local repo="$1"; shift
-	local officialImagesUrl='https://github.com/docker-library/official-images/raw/master/library/'
-
-	eval "declare -g -A parentRepoToArches=( $(
-		for version in "${versions[@]}"; do
-			if ! (echo "${dirExclude[@]}" | grep -w "\[$version\]" > /dev/null) ; then
-				find "${version}" -name 'Dockerfile' -exec awk '
-							toupper($1) == "FROM" && $2 !~ /^('"$repo"'|scratch|.*\/.*)(:|$)/ {
-								print "'"$officialImagesUrl"'" $2
-							}
-						' '{}' + \
-						| sort -u \
-						| xargs bashbrew cat --format '[{{ .RepoName }}:{{ .TagName }}]="{{ join " " .TagEntry.Architectures }}"'
-			fi
-		done
-	) )"
+    local repo="$1"
+    local officialImagesUrl='https://github.com/docker-library/official-images/raw/master/library/'
+    
+    # Build the eval string with proper escaping for associative array check
+    local evalString=""
+    for version in "${versions[@]}"; do
+        if [[ -z ${dirExclude[$version]+x} ]]; then
+            evalString+="$(
+                find "$version" -name 'Dockerfile' -exec awk '
+                    toupper($1) == "FROM" && $2 !~ /^('"$repo"'|scratch|.*\/.*)(:|$)/ {
+                        print "'"$officialImagesUrl"'" $2
+                    }
+                ' '{}' + |
+                sort -u |
+                xargs -r bashbrew cat --format '[{{ .RepoName }}:{{ .TagName }}]="{{ join " " .TagEntry.Architectures }}"'
+            ) "
+        fi
+    done
+    
+    eval "declare -g -A parentRepoToArches=( $evalString )"
 }
 getArches 'geonetwork'
 
@@ -76,60 +88,49 @@ cat <<-EOH
 # this file is generated via https://github.com/geonetwork/docker-geonetwork/blob/$(fileCommit "$self")/$self
 
 Maintainers: Joana Simoes <jo@doublebyte.net> (@doublebyte1),
-	     Juan Luis Rodriguez <juanluisrp@geocat.net> (@juanluisrp),
-		 Jose Garcia <jose.garcia@geocat.net> (@josegar74)
+         Juan Luis Rodriguez <juanluisrp@geocat.net> (@juanluisrp),
+         Jose Garcia <jose.garcia@geocat.net> (@josegar74)
 GitRepo: https://github.com/geonetwork/docker-geonetwork.git
 GitFetch: refs/heads/main
 EOH
 
-
 for version in "${versions[@]}"; do
-  if ! (echo ${dirExclude[@]} | grep -w "\[$version\]" > /dev/null) ; then
+    if [[ -z ${dirExclude[$version]+x} ]]; then
+        commit="$(dirCommit "$version")"
+        dir="$version"
+        fullVersion="$(git show "$commit":"$version/Dockerfile" | awk '$1 == "ENV" && $2 == "GN_VERSION" { print $3; exit }')"
+        versionAliases=( "$version" ${aliases[$version]:-} )
+        variantParent="$(awk 'toupper($1) == "FROM" { print $2 }' "$dir/Dockerfile")"
+        [ -n "$variantParent" ]
+        variantArches="${parentRepoToArches[$variantParent]:-}"
+        
+        echo
+        cat <<-EOE
+Tags: $(join ', ' "${versionAliases[@]}")
+Architectures: $(join ', ' $variantArches)
+GitCommit: $commit
+Directory: $version
+EOE
+        constraints="$(bashbrew cat --format '{{ .TagEntry.Constraints | join ", " }}' "https://github.com/docker-library/official-images/raw/master/library/$variantParent" 2>/dev/null || true)"
+        [ -z "$constraints" ] || echo "Constraints: $constraints"
 
-	commit="$(dirCommit "$version")"
-	dir="$version"
-
-	fullVersion="$(git show "$commit":"$version/Dockerfile" | awk '$1 == "ENV" && $2 == "GN_VERSION" { print $3; exit }')"
-
-	versionAliases=(
-		$version
-		${aliases[$version]:-}
-	)
-
-	variantParent="$(awk 'toupper($1) == "FROM" { print $2 }' "$dir/Dockerfile")"
-	[ -n "$variantParent" ]
-	variantArches="${parentRepoToArches[$variantParent]}"
-	
-	echo
-	cat <<-EOE
-		Tags: $(join ', ' "${versionAliases[@]}")
-		Architectures: $(join ', ' $variantArches)
-		GitCommit: $commit
-		Directory: $version
-	EOE
-	constraints="$(bashbrew cat --format '{{ .TagEntry.Constraints | join ", " }}' "https://github.com/docker-library/official-images/raw/master/library/$variantParent")"
-			[ -z "$constraints" ] || echo "Constraints: $constraints"
-
-	for variant in postgres; do
-		[ -f "$version/$variant/Dockerfile" ] || continue
-
-		commit="$(dirCommit "$version/$variant")"
-
-		variantAliases=( "${versionAliases[@]/%/-$variant}" )
-		variantAliases=( "${variantAliases[@]//latest-/}" )
-		variantParent="$(awk 'toupper($1) == "FROM" { print $2 }' "$dir/Dockerfile")"
-		[ -n "$variantParent" ]
-
-		echo
-		cat <<-EOE
-			Tags: $(join ', ' "${variantAliases[@]}")
-			Architectures: $(join ', ' $variantArches)
-			GitCommit: $commit
-			Directory: $version/$variant
-		EOE
-
-		constraints="$(bashbrew cat --format '{{ .TagEntry.Constraints | join ", " }}' "https://github.com/docker-library/official-images/raw/master/library/$variantParent")"
-			[ -z "$constraints" ] || echo "Constraints: $constraints"
-	done
-  fi
+        for variant in postgres; do
+            [ -f "$version/$variant/Dockerfile" ] || continue
+            commit="$(dirCommit "$version/$variant")"
+            variantAliases=( "${versionAliases[@]/%/-$variant}" )
+            variantAliases=( "${variantAliases[@]//latest-/}" )
+            variantParent="$(awk 'toupper($1) == "FROM" { print $2 }' "$version/$variant/Dockerfile")"
+            [ -n "$variantParent" ]
+            
+            echo
+            cat <<-EOE
+Tags: $(join ', ' "${variantAliases[@]}")
+Architectures: $(join ', ' $variantArches)
+GitCommit: $commit
+Directory: $version/$variant
+EOE
+            constraints="$(bashbrew cat --format '{{ .TagEntry.Constraints | join ", " }}' "https://github.com/docker-library/official-images/raw/master/library/$variantParent" 2>/dev/null || true)"
+            [ -z "$constraints" ] || echo "Constraints: $constraints"
+        done
+    fi
 done
